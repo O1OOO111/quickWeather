5f3cf2b32f370fe7490a946558ccddeffec1ad47
Godlove Damian <godlove@innocent.com>
2021-01-14 20:29:11 +0300

    refactor: refactored TabOne and TabTwo types to Forecast and Settings

--- types.tsx
+++ types.tsx
@@ -4,14 +4,14 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Forecast: undefined;
+  Settings: undefined;
 };
 
-export type TabOneParamList = {
-  TabOneScreen: undefined;
+export type ForecastParamList = {
+  Forecast: undefined;
 };
 
-export type TabTwoParamList = {
-  TabTwoScreen: undefined;
+export type SettingsParamList = {
+  Settings: undefined;
 };
