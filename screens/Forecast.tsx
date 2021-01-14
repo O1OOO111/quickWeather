0166c62ab248a01fb73ef9ea0fcff0513bc05d8c
Godlove Damian <godlove@innocent.com>
2021-01-14 20:26:14 +0300

    refactor: renamed TabOneScreen and TabTwoScreen to Forecast and Settings

--- screens/Forecast.tsx
+++ screens/Forecast.tsx
@@ -4,12 +4,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function TabOneScreen() {
+export default function WeatherForecast() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Weather Forecast</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <EditScreenInfo path="/screens/WeatherForecast.tsx" />
     </View>
   );
 }
